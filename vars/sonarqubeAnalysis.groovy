292b9ef0063d8f6ca3ddbb471c1b945a30fe8de4
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2024-01-06 17:58:13 -0600

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,12 @@
-def call() {
-    withSonarQubeEnv('sonarqube-9.9') {
+// def call() {
+//     withSonarQubeEnv('sonarqube-9.9') {
+//         sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube -Dsonar.projectKey=youtube '''
+//     }
+// }
+
+
+def call(credentialId) {
+    withSonarQubeEnv('credentialId') {
         sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube -Dsonar.projectKey=youtube '''
     }
 }
