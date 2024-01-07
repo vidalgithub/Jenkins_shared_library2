4e5eb857d27b5568cbd036fe9adcff59ca1b5fb6
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2024-01-06 19:19:43 -0600

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,10 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=f0ead79813ms -t ${imageName} ."
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=42e2928167mshd20e5fa6074323fp174f73jsn41b6161ea15d -t ${imageName} ."
      // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
-    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
+    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'dockerhub']) {
         sh "docker push ${dockerHubUsername}/${imageName}:latest"
     }
 }
